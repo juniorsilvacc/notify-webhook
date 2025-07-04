1abe4a8c36b515e0333eb82f205cb802cab7a348
juniorsilvaeng <juniorsilvacc@hotmail.com>
2025-07-04 04:58:06 -0300

    adjusting the message that is sent to whatsapp

--- webhooks/messages.py
+++ webhooks/messages.py
@@ -1,8 +1,10 @@
 outflow_message = '''
-*Olá, tivemos uma nova saída no Controle de Estoque*
+📦 *Nova saída registrada no sistema de estoque!*
 
-Produto: *{}*
-Quantidade: *{}*
-Valor da venda: *{}*
-Lucro com a venda: *{}*
+🔹 *Produto:* {}
+🔹 *Quantidade:* {}
+🔹 *Valor da venda:* R$ {:.2f}
+🔹 *Lucro estimado:* R$ {:.2f}
+
+Caso não reconheça esta movimentação, verifique no sistema.
 '''
